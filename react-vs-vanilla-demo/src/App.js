81e1a9dcf23dc830ee776f66e2bdaa6581b4b818
이종호 <purepleya@gmail.com>
2024-06-22 16:31:48 +0900

    destructuring

--- react-vs-vanilla-demo/src/App.js
+++ react-vs-vanilla-demo/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./styles.css";
-import zz, { apikey, apikey2 as ccckey } from "./util.js"
+import zz, { apikey, apikey2 as ccckey } from "./util.js";
 // import * as util from "./util.js"
 
 const content = [
@@ -8,27 +8,38 @@ const content = [
     "React is extremely popular",
     "It makes building complex, interactive UIs a breeze",
     "It's powerful & flexible",
-    "It has a very active and versatile ecosystem"
+    "It has a very active and versatile ecosystem",
   ],
   [
     "Components, JSX & Props",
     "State",
     "Hooks (e.g., useEffect())",
-    "Dynamic rendering"
+    "Dynamic rendering",
   ],
   [
     "Official web page (react.dev)",
     "Next.js (Fullstack framework)",
-    "React Native (build native mobile apps with React)"
-  ]
+    "React Native (build native mobile apps with React)",
+  ],
 ];
 
 export default function App() {
   const [activeContentIndex, setActiveContentIndex] = useState(0);
-  
+
   // console.log(apikey, apikey2, zz);
   console.log(apikey, ccckey, zz);
   // console.log(util.apikey, util.apikey2, util.default);
+
+  const [value1, value2] = ["abc", "xyz"];
+  console.log(value1, value2);
+
+  const user = {
+    name: "Max",
+    age: 34,
+  };
+
+  const { name: userName, age } = user;
+  console.log(userName, age);
 
   return (
     <div>
