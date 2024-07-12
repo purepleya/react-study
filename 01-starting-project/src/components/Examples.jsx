950c92ef403c2416fe02b49c01e2613235081a9e
이종호 <purepleya@gmail.com>
2024-07-12 09:24:49 +0900

    66. 여러 JSX 슬롯 활용법

--- 01-starting-project/src/components/Examples.jsx
+++ 01-starting-project/src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { EXAMPLES } from '../data';
 import TabButton from './TabButton'
 import Section from './Section';
+import Tabs from './Tabs';
 
 export default function Examples() {
   const [ selectedTopic, setSelectedTopic ] = useState();
@@ -25,13 +26,17 @@ export default function Examples() {
 
   return (
     <Section title="Examples" id="examples">
-      <menu>
+      <Tabs buttons={
+        <>
         <TabButton isSelected={selectedTopic === 'components'} onClick={() => handleSelect('components')}>Components</TabButton>
         <TabButton isSelected={selectedTopic === 'jsx'} onClick={() => handleSelect('jsx')}>JSX</TabButton>
         <TabButton isSelected={selectedTopic === 'props'} onClick={() => handleSelect('props')}>Props</TabButton>
         <TabButton isSelected={selectedTopic === 'state'} onClick={() => handleSelect('state')}>State</TabButton>
-      </menu>
-      {tabContent}          
+        </>
+        }>
+        {tabContent}
+      </Tabs>
+      
     </Section>
   );
 }
---
+++ 01-starting-project/src/components/Tabs.jsx
@@ -0,0 +1,6 @@
+export default function Tabs({ children, buttons }) {
+  return <>
+    <menu>{buttons}</menu>
+    {children}
+  </>;
+}
