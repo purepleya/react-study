4578604e40c36153e6563ecee998254649320667
이종호 <purepleya@gmail.com>
2024-07-13 17:47:35 +0900

    72. 새 프로젝트: 틱택토 게임 첫 단계

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,9 +2,25 @@
 function App() {
   
 
-  return (
-    <p>Comming soon...</p>
-  )
+  return <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        
+        GAME BOARD
+      </div>
+
+      LOG
+    </main>
+  ;
 }
 
 export default App
