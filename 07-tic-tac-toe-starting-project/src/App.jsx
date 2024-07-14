d8721f9a616e93482895b2184dba0b0c7d346ca7
이종호 <purepleya@gmail.com>
2024-07-14 22:40:10 +0900

    79. 다차원 리스트 렌더링

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import Player from "../components/Player";
+import GameBoard from "../components/GameBoard";
 
 function App() {
   
@@ -9,8 +10,7 @@ function App() {
           <Player initialName="Player1" symbol="X" />
           <Player initialName="Player2" symbol="O" />
         </ol>
-        
-        GAME BOARD
+        <GameBoard />
       </div>
 
       LOG
