bbcb6b30eb2ffb9378cb36adfab1c257fefa6fe5
이종호 <purepleya@gmail.com>
2024-07-16 09:11:31 +0900

    91. 틱택토 게임: “게임 오버” 화면 & 무승부 여부 확인

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,7 @@ import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations"; 
+import GameOver from "./components/GameOver";
 
 const initialGameBoard = [
   [null, null, null],
@@ -45,6 +46,8 @@ function App() {
     }
   }
 
+  const hasDraw = gameTurns.length === 9 && !winner
+
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
     setGameTurns(prevTurns => {
@@ -65,7 +68,7 @@ function App() {
           <Player initialName="Player1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        {winner && <p> You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns}/>
