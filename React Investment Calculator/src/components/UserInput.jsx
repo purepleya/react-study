8e003e1d74ed4c3c02a4cca8b357364f4386702f
jh <purepleya@gmail.com>
2024-07-16 23:37:37 +0900

    99. 이벤트 핸들링 & 양방향 바인딩 활용

--- React Investment Calculator/src/components/UserInput.jsx
+++ React Investment Calculator/src/components/UserInput.jsx
@@ -1,23 +1,42 @@
+import { useState } from "react";
+
 export default function UserInput() {
+  const [useInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...useInput,
+        [inputIdentifier]: newValue
+      }      
+    });
+  
+  }
+
 	return <section id="use-input">
     <div className="input-group">
       <p>
         <label>Initial Investment</label>
-        <input type="number" required />
+        <input type="number" required value={UserInput.initialInvestment} onChange={(event) => handleChange('initialInvestment', event.target.value)} />
       </p>
       <p>
         <label>Annual Investment</label>
-        <input type="number" required />
+        <input type="number" required value={UserInput.annualInvestment} onChange={(event) => handleChange('annualInvestment', event.target.value)} />
       </p>
     </div>
     <div className="input-group">
       <p>
         <label>Expected Return</label>
-        <input type="number" required />
+        <input type="number" required value={UserInput.expectedReturn} onChange={(event) => handleChange('expectedReturn', event.target.value)} />
       </p>
       <p>
         <label>Duration</label>
-        <input type="number" required />
+        <input type="number" required value={UserInput.duration} onChange={(event) => handleChange('duration', event.target.value)} />
       </p>
     </div>
   </section>;
