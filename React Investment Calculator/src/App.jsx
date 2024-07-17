1a80d10e8d9437f71e48d37727b9c0dc233e40e9
이종호 <purepleya@gmail.com>
2024-07-17 19:52:47 +0900

    103. 조건적 콘텐츠 출력

--- React Investment Calculator/src/App.jsx
+++ React Investment Calculator/src/App.jsx
@@ -5,11 +5,13 @@ import Results from './components/Results';
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 15000,
+    initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10
   });
+
+  const inputIsValid = userInput.duration >= 1;
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
@@ -24,7 +26,8 @@ function App() {
   return <>
     <Header />
     <UserInput onChange={handleChange} userInput={userInput} />
-    <Results input={userInput} />
+    {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+    {inputIsValid && <Results input={userInput} />}
   </>;
     
 }
