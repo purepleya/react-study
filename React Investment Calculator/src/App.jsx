c7eff9e60bedb5348d07556b788d15dee3979bfa
이종호 <purepleya@gmail.com>
2024-07-17 19:23:45 +0900

    102. 리스트에 결과 출력하기 및 더 많은 값 파생하기

--- React Investment Calculator/src/App.jsx
+++ React Investment Calculator/src/App.jsx
@@ -5,7 +5,7 @@ import Results from './components/Results';
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
+    initialInvestment: 15000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10
--- React Investment Calculator/src/components/Results.jsx
+++ React Investment Calculator/src/components/Results.jsx
@@ -1,10 +1,32 @@
-import { calculateInvestmentResults } from '../util/investment'
+import { calculateInvestmentResults, formatter } from '../util/investment'
 
 export default function Results({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment;
   
-  const result = calculateInvestmentResults(input);
-
-  console.log(result);
-
-  return <p>Results...</p>;
+  return <table id="result">
+    <thead>
+      <tr>
+        <th>Year</th>
+        <th>Investment Value</th>
+        <th>Interest (Year)</th>
+        <th>Total Interest</th>
+        <th>Invested Capital</th>
+      </tr>
+    </thead>
+    <tbody>
+      {resultsData.map(yearData => {
+        const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year - initialInvestment;
+        const totlaAmountInvested = yearData.valueEndOfYear - totalInterest;
+        
+        return <tr key={yearData.year}>
+          <td>{yearData.year}</td>
+          <td>{formatter.format(yearData.valueEndOfYear)}</td>
+          <td>{formatter.format(yearData.interest)}</td>
+          <td>{formatter.format(totalInterest)}</td>
+          <td>{formatter.format(totlaAmountInvested)}</td>
+        </tr>
+      })}
+    </tbody>
+  </table>;
 }
