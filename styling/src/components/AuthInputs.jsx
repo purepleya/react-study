0b9bdb3a5096fc083ca3936884a7c99f50665664
이종호 <purepleya@gmail.com>
2024-07-19 15:18:52 +0900

    111. CSS 모듈로 CSS 규칙 스코핑하기

--- styling/src/components/AuthInputs.jsx
+++ styling/src/components/AuthInputs.jsx
@@ -23,7 +23,7 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <div className="controls">
-        <p>
+        <p className='paragraph'>
           <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
             type="email"
@@ -32,7 +32,7 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Password</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
