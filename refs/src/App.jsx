e5ae7a21fc1c95e327d41ec1f7ac6bcf12352577
이종호 <purepleya@gmail.com>
2024-07-24 18:15:40 +0900

    135. 데모 프로젝트에 도전 과제 추가하기

--- refs/src/App.jsx
+++ refs/src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimmerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimmerChallenge title="Easy" targetTime={1} />
+        <TimmerChallenge title="Not easy" targetTime={5} />
+        <TimmerChallenge title="Getting tough" targetTime={10} />
+        <TimmerChallenge title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
