e347b94e3928fbd05e932a5c1dd26d084ada4f09
이종호 <purepleya@gmail.com>
2024-07-24 18:23:57 +0900

    136. 타이머 설정 & State(상태) 관리

--- refs/src/components/TimerChallenge.jsx
+++ refs/src/components/TimerChallenge.jsx
@@ -1,18 +1,34 @@
+import { useState } from 'react';
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+      
+  }
   
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You Lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>
-          Start Callenge
+        <button onClick={handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} Callenge
         </button>
       </p>
-      <p className="">
-        Time is running... / Timer inactive
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Time is running...' : 'Timer inactive'}
       </p>
     </section>
   );
