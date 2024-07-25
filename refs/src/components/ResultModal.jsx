0e36fa4f0a029c5719e332563dcc5e92873475fe
이종호 <purepleya@gmail.com>
2024-07-25 19:39:51 +0900

    142. 컴포넌트 간의 State(상태) 공유

--- refs/src/components/ResultModal.jsx
+++ refs/src/components/ResultModal.jsx
@@ -1,15 +1,15 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formmatedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
       open() {
         dialog.current.showModal();
-      },
-      close() {
-        // close
       }
     };
   });
@@ -17,10 +17,10 @@ const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref)
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>Your {result}</h2>
+      {userLost && <h2>Your lost</h2>}
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
-      <p>You stopped the timer with <strong>X seconds left.</strong></p>
-      <form method="dialog">
+      <p>You stopped the timer with <strong>{formmatedRemainingTime} seconds left.</strong></p>
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
--- refs/src/components/TimerChallenge.jsx
+++ refs/src/components/TimerChallenge.jsx
@@ -10,8 +10,11 @@ export default function TimerChallenge({ title, targetTime }) {
 
   if (timeRemaining <= 0) {
     clearInterval(timer.current);
-    setTimeRemaining(targetTime * 1000);
     dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStart() {
@@ -28,10 +31,10 @@ export default function TimerChallenge({ title, targetTime }) {
   
   return (
     <>
-      <ResultModal ref={dialog} timerTime={targetTime} result="lost" />
+      <ResultModal ref={dialog} timerTime={targetTime} result="lost" remainingTime={timeRemaining} onReset={handleReset} />
       <section className="challenge">
         <h2>{title}</h2>
-        {timerExpired && <p>You Lost!</p>}
+        {!timerIsActive && <p>You Lost!</p>}
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
