d6fe056f10ccd642b78c9cdf41609552fc2eb1d5
jh <purepleya@gmail.com>
2024-07-26 21:24:09 +0900

    153. Refs(참조)와 전달된 Refs(참조)로 사용자 입력 받아오기

--- 09 Practice Project - Project Management/src/components/NewProject.jsx
+++ 09 Practice Project - Project Management/src/components/NewProject.jsx
@@ -1,17 +1,48 @@
+import { useRef } from 'react';
+
 import Input from "./Input";
 
-export default function NewProject() {
-  
+
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enterTitle = title.current.value;
+    const enterDescription = description.current.value;
+    const enterDueDate = dueDate.current.value;
+
+    // validation ...
+
+    onAdd({
+      title: enterTitle,
+      description: enterDescription,
+      dueDate: enterDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex item-center justify-end gap-4 my-4">
-        <li><button className="text-stone-100 hover:text-stone-950">Cancel</button></li>
-        <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950 ">Save</button></li>
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button 
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
+            Save
+          </button>
+        </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input type="text" ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
   );
