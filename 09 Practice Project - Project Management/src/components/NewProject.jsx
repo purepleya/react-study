b6afa5089b5d423dd4da7c18c04658ab9113937d
jh <purepleya@gmail.com>
2024-07-27 22:12:07 +0900

    155. 사용자 입력 유효성 검사 & useImperativeHandle로 에러 모달 띄우기

--- 09 Practice Project - Project Management/src/components/NewProject.jsx
+++ 09 Practice Project - Project Management/src/components/NewProject.jsx
@@ -1,9 +1,12 @@
 import { useRef } from 'react';
 
 import Input from "./Input";
+import Modal from './Modal';
 
 
 export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -13,7 +16,13 @@ export default function NewProject({ onAdd }) {
     const enterDescription = description.current.value;
     const enterDueDate = dueDate.current.value;
 
-    // validation ...
+    if (enterTitle.trim() === '' ||
+      enterDescription.trim() === '' ||
+      enterDueDate.trim() === ''
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enterTitle,
@@ -23,27 +32,34 @@ export default function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex item-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button 
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input type="date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Okay" >
+        <h2>Invalid Input</h2>
+        <p>Oops ... looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button 
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
