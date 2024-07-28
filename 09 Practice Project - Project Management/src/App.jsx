7d07bc420c690aa3ab1f5eb4b04b62076d40d342
jh <purepleya@gmail.com>
2024-07-28 10:41:38 +0900

    157. 선택 가능한 프로젝트 구현 및 프로젝트 정보 보기

--- 09 Practice Project - Project Management/src/App.jsx
+++ 09 Practice Project - Project Management/src/App.jsx
@@ -2,12 +2,22 @@ import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/PorjectSidebar";
+import SelectedProject from "./components/SelectedProject";
 
 function App() {
   const [projectState, setProjectState] = useState({
     selectedProjectId: undefined,
     projects: [],
   });
+
+  function handleSelectProject(id) {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id
+      };
+    });
+  }
 
   function handleStartAddProject() {
     setProjectState(prevState => {
@@ -42,20 +52,22 @@ function App() {
     });
   }
 
-  
+  const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId);
 
-  let content;
+  let content = <SelectedProject project={selectedProject} />;
   if (projectState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
-  }
+  } 
 
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectState.projects}
+        onSelectProject={handleSelectProject}
+        selectedProjectId={projectState.selectedProjectId}
       />
       {content}
     </main>
