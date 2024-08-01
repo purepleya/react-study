d0f4f7863aa21eb4e874b9692c4d2dfef7ce8d7b
이종호 <purepleya@gmail.com>
2024-08-01 09:59:43 +0900

    174. Action 보내기 & useReducer로 State(상태) 수정하기

--- 10 Advanced State Management with Context useReducer/src/store/shopping-cart-context.jsx
+++ 10 Advanced State Management with Context useReducer/src/store/shopping-cart-context.jsx
@@ -8,27 +8,11 @@ export const CartContext = createContext({
 });
 
 function shippingCartReducer(state, action) {
-  return state;
-}
-
-export default function CartContextProvider({children}) {
-  const [ shippingCartState, shippingCartDispatch ] = useReducer(
-    shippingCartReducer,
-    {
-      items: [],
-    }
-  );
-
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
-
-  function handleAddItemToCart(id) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
+  if (action.type === 'ADD_ITEM') {
+    const updatedItems = [...state.items];
 
       const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
+        (cartItem) => cartItem.id === action.payload
       );
       const existingCartItem = updatedItems[existingCartItemIndex];
 
@@ -39,9 +23,9 @@ export default function CartContextProvider({children}) {
         };
         updatedItems[existingCartItemIndex] = updatedItem;
       } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
         updatedItems.push({
-          id: id,
+          id: action.payload,
           name: product.title,
           price: product.price,
           quantity: 1,
@@ -49,23 +33,22 @@ export default function CartContextProvider({children}) {
       }
 
       return {
+        ...state,
         items: updatedItems,
       };
-    });
   }
 
-  function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
+  if (action.type === 'UPDATE_ITEM') {
+    const updatedItems = [...state.items];
       const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
+        (item) => item.id === action.payload.productId
       );
 
       const updatedItem = {
         ...updatedItems[updatedItemIndex],
       };
 
-      updatedItem.quantity += amount;
+      updatedItem.quantity += action.payload.amount;
 
       if (updatedItem.quantity <= 0) {
         updatedItems.splice(updatedItemIndex, 1);
@@ -74,9 +57,38 @@ export default function CartContextProvider({children}) {
       }
 
       return {
+        ...state,
         items: updatedItems,
       };
+  }
+
+  return state;
+}
+
+export default function CartContextProvider({children}) {
+  const [ shoppingCartState, shoppingCartDispatch ] = useReducer(
+    shippingCartReducer,
+    {
+      items: [],
+    }
+  );
+
+  function handleAddItemToCart(id) {
+    shoppingCartDispatch({
+      type: 'ADD_ITEM', //action type
+      payload: id
     });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    shoppingCartDispatch({
+      type: 'UPDATE_ITEM',
+      payload: {
+        // productId: productId,
+        // amount: amount
+        productId, amount
+      }
+    })
   }
 
   const ctxValue = {
