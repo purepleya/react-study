c005d8e77075042648e52b55091bad830c55b9c5
jh <purepleya@gmail.com>
2024-08-02 16:58:39 +0900

    181. useEffect를 활용하는 다른 적용 사례

--- 11 Side Effects useEffect/src/components/Modal.jsx
+++ 11 Side Effects useEffect/src/components/Modal.jsx
@@ -1,19 +1,10 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
+export default function Modal({ open, children }) {
   const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
+
 
   return createPortal(
     <dialog className="modal" ref={dialog}>
@@ -21,6 +12,4 @@ const Modal = forwardRef(function Modal({ children }, ref) {
     </dialog>,
     document.getElementById('modal')
   );
-});
-
-export default Modal;
+}
