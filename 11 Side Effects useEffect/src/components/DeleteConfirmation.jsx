1707ee41ce86e54c4cb15727f2bdff1d6e09c40d
jh <purepleya@gmail.com>
2024-08-02 21:56:04 +0900

    190. State(상태) 업데이트 최적화

--- 11 Side Effects useEffect/src/components/DeleteConfirmation.jsx
+++ 11 Side Effects useEffect/src/components/DeleteConfirmation.jsx
@@ -1,20 +1,9 @@
 import { useEffect, useState } from "react";
+import ProgressBar from "./ProgressBar";
 
 const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const [remainingTime, setRemainingTime]  = useState(TIMER);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      console.log('INTERVAL')
-      setRemainingTime(prevTime => prevTime - 10);
-    }, 10);
-
-    return () => {
-      clearInterval(interval);
-    }
-  }, []);
 
   useEffect(() => {
     console.log('TIMER SET')
@@ -42,7 +31,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
