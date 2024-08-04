05bad314cfb555ddb97b8b889e05bf526739525f
이종호 <purepleya@gmail.com>
2024-08-04 21:08:21 +0900

    196. Effect 의존성 & useCallback 활용법

--- 13 Demo Project - React Quiz/src/components/Quiz.jsx
+++ 13 Demo Project - React Quiz/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import quizCompleteImg from '../assets/quiz-complete.png';
 
 import QUESTIONS from '../questions.js';
@@ -10,11 +10,13 @@ export default function Quiz() {
   const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
       return [...prevAnswers, selectedAnswer];
     });
-  }
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
   if (quizIsComplete) {
     return  <div id="summary">
@@ -29,7 +31,7 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
-        <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}/>
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer}/>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map (answer => (
