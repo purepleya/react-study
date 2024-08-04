2303b54054e613938ac560dd1f24bbbebd8eb43e
이종호 <purepleya@gmail.com>
2024-08-04 23:52:07 +0900

    201. 선택된 답변 기반 타이머 설정

--- 13 Demo Project - React Quiz/src/components/QuestionTimer.jsx
+++ 13 Demo Project - React Quiz/src/components/QuestionTimer.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 
-export default function QuestionTimer({ timeout, onTimeout }) {
+export default function QuestionTimer({ timeout, onTimeout, mode }) {
   const [remainingTime, setRemainingTime] = useState(timeout); 
   
   
@@ -24,6 +24,6 @@ export default function QuestionTimer({ timeout, onTimeout }) {
   
 
   return (
-    <progress id="question-timer" max={timeout} value={remainingTime} />
+    <progress id="question-timer" max={timeout} value={remainingTime} className={mode} />
   );
 }
