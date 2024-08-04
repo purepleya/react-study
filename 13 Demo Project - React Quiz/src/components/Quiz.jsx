4a65415235b3d03ba59b1566cc5adc5a49684f8d
이종호 <purepleya@gmail.com>
2024-08-04 22:33:47 +0900

    199. 컴포넌트 분리를 통해 문제 해결하기

--- 13 Demo Project - React Quiz/src/components/Quiz.jsx
+++ 13 Demo Project - React Quiz/src/components/Quiz.jsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from 'react';
 import quizCompleteImg from '../assets/quiz-complete.png';
 
 import QUESTIONS from '../questions.js';
-import QuestionTimer from './QuestionTimer.jsx';
+import Question from './Question.jsx';
 
 export default function Quiz() {
   const [answerState, setAnswerState] = useState('');
@@ -39,38 +39,19 @@ export default function Quiz() {
     </div>
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
+  
 
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer 
-          key={activeQuestionIndex}
-          timeout={10000} 
-          onTimeout={handleSkipAnswer}
-        />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map (answer => {
-            const isSelected = userAnswers[userAnswers.length - 1] === answer;
-            let cssClasses = '';
-            
-            if (answerState === 'answered' && isSelected) {
-              cssClasses = 'selected';
-            }
-
-            if ((answerState === 'correct' || answerState === 'wrong') && isSelected) {
-              cssClasses = answerState;
-            }
-
-            return <li key={answer} className='answer'>
-              <button onClick={() => handleSelectAnswer(answer)} className={cssClasses} >{answer}</button>
-            </li>;
-          }
-          )}
-        </ul>
-      </div>
+      <Question 
+        key={activeQuestionIndex}
+        questionText={QUESTIONS[activeQuestionIndex].text}
+        answers={QUESTIONS[activeQuestionIndex].answers}
+        onSelectAnswer={handleSelectAnswer}
+        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        answerState={answerState}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
