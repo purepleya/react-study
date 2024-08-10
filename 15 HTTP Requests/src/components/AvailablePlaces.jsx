476a3f2a82d6a9f3093bf8709e07a5c98d70b7c8
이종호 <purepleya@gmail.com>
2024-08-10 23:25:01 +0900

    234. HTTP 에러 다루기

--- 15 HTTP Requests/src/components/AvailablePlaces.jsx
+++ 15 HTTP Requests/src/components/AvailablePlaces.jsx
@@ -1,23 +1,40 @@
 import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+
+      try {
+        const response = await fetch('http://localhost:3000/placesss');
+        const resData = await response.json();
+        
+        if (!response.ok) {
+          throw new Error('Failt to fetch places');
+        }
+
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({message: error.message || 'Cloud not fetch places, please try again later.'});
+      }
+
       setIsFetching(false);
     }
     
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />
+  }
 
   return (
     <Places
