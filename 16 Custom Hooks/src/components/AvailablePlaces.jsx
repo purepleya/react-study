1cf0b3c28eb2ab4d155469b51794f8c480c7b8b5
jh <purepleya@gmail.com>
2024-08-12 23:45:56 +0900

    246. 다중 컴포넌트에서 커스텀 Hook(훅) 사용하기

--- 16 Custom Hooks/src/components/AvailablePlaces.jsx
+++ 16 Custom Hooks/src/components/AvailablePlaces.jsx
@@ -4,35 +4,34 @@ import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
+  const {isFetching, error, fetchedData: availablePlaces, setFetchedData: setAvailablePlaces} = useFetch(fetchAvailablePlaces, []);
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
 
-      try {
-        const places = await fetchAvailablePlaces();
+      // try {
+      //   const places = await fetchAvailablePlaces();
 
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({
-          message:
-            error.message || 'Could not fetch places, please try again later.',
-        });
-        setIsFetching(false);
-      }
+      //   navigator.geolocation.getCurrentPosition((position) => {
+      //     const sortedPlaces = sortPlacesByDistance(
+      //       places,
+      //       position.coords.latitude,
+      //       position.coords.longitude
+      //     );
+      //     setAvailablePlaces(sortedPlaces);
+      //     setIsFetching(false);
+      //   });
+      // } catch (error) {
+      //   setError({
+      //     message:
+      //       error.message || 'Could not fetch places, please try again later.',
+      //   });
+      //   setIsFetching(false);
+      // }
     }
 
     fetchPlaces();
