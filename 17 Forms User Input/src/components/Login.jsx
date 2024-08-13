91416cc68826359e4ad110c7ce936660eeaa6fb0
jh <purepleya@gmail.com>
2024-08-13 22:44:06 +0900

    251. State(상태) & 일반 Handler로 사용자 입력 수집 및 관리

--- 17 Forms User Input/src/components/Login.jsx
+++ 17 Forms User Input/src/components/Login.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  const [enterdValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
+
   function handleSubmit(event) {
     event.preventDefault(); // 기본구성, 폼을 전송하는 HTTP 요청을 중단
-    console.log('Submitted');
+    console.log(enterdValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues(preValues => ({
+      ...preValues,
+        [identifier]: value
+    }))
   }
 
   return (
@@ -11,12 +27,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(event) => handleInputChange('email', event.target.value)} value={enterdValues.email}/>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(event) => handleInputChange('password', event.target.value)} value={enterdValues.password} />
         </div>
       </div>
 
