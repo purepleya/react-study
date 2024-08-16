e874f141169f11c6608e54d74f951c3eca53428b
이종호 <purepleya@gmail.com>
2024-08-16 21:43:27 +0900

    257. Form 제출시 입력 유효성 검사

--- 17 Forms User Input/src/components/Login.jsx
+++ 17 Forms User Input/src/components/Login.jsx
@@ -1,15 +1,26 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault(); // 기본구성, 폼을 전송하는 HTTP 요청을 중단
+    
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+    console.log('Sending HTTP request...');
   }
 
   return (
@@ -20,6 +31,7 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address.</p>}</div>
         </div>
 
         <div className="control no-margin">
