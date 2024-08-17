8d9883aec36cb50dbd8a0f75c3646fd43fed9cb4
이종호 <purepleya@gmail.com>
2024-08-17 17:39:02 +0900

    262. 커스텀 useInput Hook(훅) 생성

--- 17 Forms User Input/src/components/StateLogin.jsx
+++ 17 Forms User Input/src/components/StateLogin.jsx
@@ -1,68 +1,51 @@
-import { useState } from 'react';
 import Input from './Input';
 import { isEmail, isNotEmpty, hasMinLength } from '../util/validation.js';
+import { useInput } from '../hooks/useInput.js';
 
 export default function Login() {
-  // const [enteredEmail, setEnteredEmail] = useState('');
-  // const [enteredPassword, setEnteredPassword] = useState('');
-  const [enteredValues, setEnteredValues] = useState({
-    email: '',
-    password: ''
-  });
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError
+  } = useInput('', (value) => isEmail(value) && isNotEmpty(value));
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false
-  });
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError
+  } = useInput('', (value) => hasMinLength(value, 6));
 
-
-  const emailIsInvalid = 
-    didEdit.email && 
-    !isEmail(enteredValues.email) && 
-    !isNotEmpty(enteredValues.email);
-  const passwordIsInvalid = 
-    didEdit.password && 
-    !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event) {
-    event.preventDefault(); // 기본구성, 폼을 전송하는 HTTP 요청을 중단
-    console.log(enteredValues);
+    event.preventDefault();
+
+    if (emailHasError || passwordHasError) {
+      return;
+    }
+
+    console.log(emailValue, passwordValue);
   }
 
-  function handleInputChange(identifier, value) {
-    setEnteredValues(preValues => ({
-      ...preValues,
-        [identifier]: value
-    }))
-    setDidEdit(prevEdit => ({
-      ...prevEdit,
-      [identifier]: false
-    }));
-  }
-
-  function handleInputBlur(identifier) {
-    setDidEdit(prevEdit => ({
-      ...prevEdit,
-      [identifier]: true
-    }));
-  }
+  
 
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
       <div className="control-row">
         <Input label="Email"  id="email" type="email" name="email" 
-          onBlur={() => handleInputBlur('email')}
-          onChange={(event) => handleInputChange('email', event.target.value)}
-          value={enteredValues.email}
-          error={emailIsInvalid && 'Please enter a valid email!'}
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && 'Please enter a valid email!'}
         />
 
         <Input label="Password"  id="password" type="password" name="password" 
-          onBlur={() => handleInputBlur('password')}
-          onChange={(event) => handleInputChange('password', event.target.value)}
-          value={enteredValues.password}
-          error={passwordIsInvalid && 'Please enter a valid password!'}
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && 'Please enter a valid password!'}
         />
       </div>
 
