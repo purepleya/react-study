ca43fc29f305f81801af5bbaf2392ed405e56d3a
이종호 <purepleya@gmail.com>
2024-08-17 15:16:15 +0900

    259. 커스텀과 내장 검증 로직 혼합

--- 17 Forms User Input/src/components/Signup.jsx
+++ 17 Forms User Input/src/components/Signup.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 export default function Signup() {
+  const [passwordsAreNotEqual, setPasswordsArNotEqual] = useState(false);
+
   function handleSubmit(event) {
     event.preventDefault();
 
@@ -6,6 +10,12 @@ export default function Signup() {
     const acquisitionChannel = fd.getAll('acquisition');
     const data = Object.fromEntries(fd.entries());
     data.acquisition = acquisitionChannel;
+
+    if (data.password !== data['confirm-password']) {
+      setPasswordsArNotEqual(true);
+      return;
+    }
+
     console.log(data);
   }
 
@@ -31,7 +41,9 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            required
           />
+          <div className='control-error'>{passwordsAreNotEqual && <p>Passwords must match.</p>}</div>
         </div>
       </div>
 
