b206635c2c2f70bb7f6090882bf47f987c45e8d3
이종호 <purepleya@gmail.com>
2024-08-18 16:58:18 +0900

    276. Form 제출과 유효성 검사 관리

---
+++ 18 Practice Project - Food Order/src/components/Checkout.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import CartContext from "../store/CartContext";
+import { Modal } from "./UI/Modal";
+import { currencyFormatter } from "../util/formatting"; 
+import Input from "./UI/Input";
+import UserProgressContext from "../store/UserProgressContext";
+import Button from "./UI/Button";
+
+export default function Checkout() {
+  const cartCtx = useContext(CartContext); 
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const cartTotal = cartCtx.items.reduce(
+    (totalPrice, item) => totalPrice + (item.price * item.quantity),
+     0
+  );
+
+  function handleClose() {
+    userProgressCtx.hideCheckout();
+  }
+
+  return (
+    <Modal open={userProgressCtx.progress === 'checkout'} onClose={handleClose}>
+      <form>
+        <h2>Checkout</h2>
+        <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
+        <Input lable="Full Name" type="text" id="full-name" />
+        <Input label="E-Mail Address" type="email" id="email" />
+        <Input lable="Street" type="text" id="street" />
+        
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+
+        <p className="modal-actions">
+          <Button type="button" textOnly onClick={handleClose}>Close</Button>
+          <Button>Submit Order</Button>
+        </p>
+      </form>
+    </Modal>
+  );
+}
--- 18 Practice Project - Food Order/src/components/UI/Modal.jsx
+++ 18 Practice Project - Food Order/src/components/UI/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-export function Modal({children, open, className = ''}) {
+export function Modal({children, open, onClose, className = ''}) {
   const dialog = useRef();
 
   useEffect(() => {
@@ -10,12 +10,18 @@ export function Modal({children, open, className = ''}) {
     if (open) {
       modal.showModal();
     }
-    
+
     return () => modal.close();
   }, [open]);
 
   return createPortal(
-    <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
+    <dialog
+      ref={dialog}
+      className={`modal ${className}`}
+      onClose={onClose}
+    >
+      {children}
+    </dialog>,
     document.getElementById('modal')
   );
 }
