9596c85ac28b7d2d71fb8b914ed26764e9e0cef3
이종호 <purepleya@gmail.com>
2024-08-18 13:43:52 +0900

    271. Cart 컨텍스트와 Reducer 끝내기 및 활용

--- 18 Practice Project - Food Order/src/store/CartContext.jsx
+++ 18 Practice Project - Food Order/src/store/CartContext.jsx
@@ -30,15 +30,48 @@ function cartReducer(state, action) {
   }
 
   if (action.type === 'REMOVE_ITEM') {
-    // removean item from the state
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    if (existingCartItem.quantity === 1) {
+      const updatedItems = [...state.items];
+      updatedItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1
+      };
+
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {...state, items: updatedItems};
   }
 
   return state;
 }
 
 export function CartContextProvider({children}) {
-  useReducer(cartReducer, { items: [] });
-  return <CartContext.Provider>{children}</CartContext.Provider>
+  const [cart, dispachCartAction] = useReducer(cartReducer, { items: [] });
+
+
+  function addItem(item) {
+    dispachCartAction({type: 'ADD_ITEM', item});
+  }
+  
+  function removeItem(id) {
+    dispachCartAction({type: 'REMOVE_ITEM', id});
+  }
+  
+  const cartContext = {
+    items: cart.items,
+    addItem,
+    removeItem
+  }
+  
+  return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
 }
 
 export default CartContext;
