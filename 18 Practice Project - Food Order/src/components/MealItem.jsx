f817a0f899c2858f41f79eef70142520b46c56ff
이종호 <purepleya@gmail.com>
2024-08-18 12:52:45 +0900

    268. 숫자를 통화 형식으로 변환 및 측정

--- 18 Practice Project - Food Order/src/components/MealItem.jsx
+++ 18 Practice Project - Food Order/src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import {currencyFormatter} from '../util/formatting.js';
+
 export default function MealItem({ meal }) {
 
   return (
@@ -6,7 +8,9 @@ export default function MealItem({ meal }) {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meail-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
