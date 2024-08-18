1d141102f19a9d32716fede4958c7232140a86cd
이종호 <purepleya@gmail.com>
2024-08-18 16:29:56 +0900

    274. Cart Items 작업

--- 18 Practice Project - Food Order/src/store/CartContext.jsx
+++ 18 Practice Project - Food Order/src/store/CartContext.jsx
@@ -30,13 +30,13 @@ function cartReducer(state, action) {
   }
 
   if (action.type === 'REMOVE_ITEM') {
-    const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
+    const updatedItems = [...state.items];
+    const existingCartItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.id
     );
 
-    const existingCartItem = state.items[existingCartItemIndex];
+    const existingCartItem = updatedItems[existingCartItemIndex];
     if (existingCartItem.quantity === 1) {
-      const updatedItems = [...state.items];
       updatedItems.splice(existingCartItemIndex, 1);
     } else {
       const updatedItem = {
