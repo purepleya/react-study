9338fb98b6283a45127bb5665368d54034570bb8
이종호 <purepleya@gmail.com>
2024-08-25 20:41:04 +0900

    315. 액션 생성자 Thunk 사용하기

--- 20 Advenced Redux/src/store/cart-slice.js
+++ 20 Advenced Redux/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -32,6 +33,47 @@ const cartSlice = createSlice({
     },
   }
 });
+
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending...',
+      message: 'Sending cart data!'
+    }));
+
+    const sendRequest = async() => {
+      const response = await fetch(
+        'https://react-http-6b4a6.firebaseio.com/cart.json',
+         { method: 'PUT', body: JSON.stringify(cart) }
+      );
+  
+      if (!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+    }
+    
+    try {
+      await sendRequest();
+
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!'
+      }));
+
+    } catch (error) {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!'
+      }));
+    }
+
+  };
+};
+
 
 export const cartActions = cartSlice.actions;
 export default cartSlice;
