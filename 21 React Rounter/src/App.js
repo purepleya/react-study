0cf6932531e021e8698b696d3ffd13b6dfaf68b3
이종호 <purepleya@gmail.com>
2024-08-26 18:45:18 +0900

    328. 레이아웃 및 중첩된 라우트

--- 21 React Rounter/src/App.js
+++ 21 React Rounter/src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 // const routeDefinitions = createRoutesFromElements(
 //   <Route>
@@ -12,8 +13,13 @@ import ProductsPage from './pages/Products';
 // const router = createBrowserRouter(routeDefinitions);
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> },
+  { path: '/',
+    element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ]
+  },  
 ]);
 
 function App() {
