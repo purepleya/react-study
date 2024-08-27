0394069a5360c2152262b47a8d32d22a03edd490
이종호 <purepleya@gmail.com>
2024-08-27 22:48:25 +0900

    333. 동적 라우트에 링크 추가하기

--- 21 React Rounter/src/pages/Products.js
+++ 21 React Rounter/src/pages/Products.js
@@ -1,10 +1,22 @@
+import { Link } from 'react-router-dom';
+
+const PRODUCTS = [
+  {id: 'p1', title: 'Product1'},
+  {id: 'p2', title: 'Product2'},
+  {id: 'p3', title: 'Product3'},
+];
+
 function ProductsPage() {
   return <>
     <h1>The Product Page</h1>
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      <li>
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={`/products/${prod.id}`}>{prod.title}</Link>
+          </li>
+        ))}
+      </li>
     </ul>
   </>
 }
