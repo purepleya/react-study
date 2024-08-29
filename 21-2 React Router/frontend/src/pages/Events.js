49db99c627e0d10d1676b0dfd580b14996e692a3
이종호 <purepleya@gmail.com>
2024-08-29 23:20:44 +0900

    338. 연습 시간: 해설

---
+++ 21-2 React Router/frontend/src/pages/Events.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {id: 'e1', title: 'Some event'},
+  {id: 'e2', title: 'Another event'}
+];
+
+function EventPage() {
+  return (
+    <>
+      <h1>EventPage</h1>
+      <ul>
+        {DUMMY_EVENTS.map(event => <li key={event.id}>
+          <Link to={event.id}>{event.title}</Link>
+        </li>)}
+      </ul>
+    </>
+  );
+}
+
+export default EventPage;
---
+++ 21-2 React Router/frontend/src/pages/NewEvent.js
@@ -0,0 +1,7 @@
+function NewEventPage() {
+  return (
+    <h1>NewEventPage</h1>
+  );
+}
+
+export default NewEventPage;
