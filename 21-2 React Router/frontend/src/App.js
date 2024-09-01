d7cedec5296d7562adde3f99ae02be692f5d4a05
이종호 <purepleya@gmail.com>
2024-09-01 22:29:18 +0900

    357. request.method로 액션 재사용하기

--- 21-2 React Router/frontend/src/App.js
+++ 21-2 React Router/frontend/src/App.js
@@ -31,11 +31,12 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import HomePage from './pages/Home';
 import EventPage, { loader as eventLoader} from './pages/Events';
 import EventDetailPage, { loader as eventDetailLoader, action as deleteEventAction} from './pages/EventDetail';
-import NewEventPage, { action as newEventAction } from './pages/NewEvent';
+import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
 import EventRootLayout from './pages/EventRoot';
 import ErrorPage from './pages/Error';
+import { action as manipulateEventAction } from './components/EventForm'
 
 const router = createBrowserRouter([
   { 
@@ -52,10 +53,10 @@ const router = createBrowserRouter([
             loader: eventDetailLoader,
             children: [
               { index: true, element: <EventDetailPage />, action: deleteEventAction},
-              { path: 'edit', element: <EditEventPage /> },
+              { path: 'edit', element: <EditEventPage />, action: manipulateEventAction},
             ]
           },
-          { path: 'new', element: <NewEventPage />, action: newEventAction },
+          { path: 'new', element: <NewEventPage />, action: manipulateEventAction },
         ]
       }
     ]
