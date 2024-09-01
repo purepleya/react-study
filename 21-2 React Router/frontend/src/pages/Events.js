9afcc99a25bf4eaa14781d5b062082a80cc5f967
이종호 <purepleya@gmail.com>
2024-09-01 23:06:44 +0900

    359. defer() 함수로 데이터 가져오기를 연기하는 방법

--- 21-2 React Router/frontend/src/pages/Events.js
+++ 21-2 React Router/frontend/src/pages/Events.js
@@ -1,22 +1,22 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
-  const data = useLoaderData();
-  // if (data.isError) {
-  //   return <p>{data.message}</p>
-  // }
-  const events = data.events;
-
+  const { events } = useLoaderData();
   return (
-    <EventsList events={events} />
+    <Suspense fallback={<p style={{textAlign:'center'}}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
 
-export async function loader() {
+export async function loadeEvents() {
   const response = await fetch('http://localhost:8080/events');
   
   if (!response.ok) {
@@ -24,6 +24,14 @@ export async function loader() {
     // throw new Response(JSON.stringify({ message: 'Could not fetch events.'}), {status: 500});
     throw json({ message: 'Could not fetch events.'}, {status: 500});
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+
+export function loader() {
+  return defer({
+    events: loadeEvents()
+  });
 }
