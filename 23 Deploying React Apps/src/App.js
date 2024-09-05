aa837b6fd3b2cdad8e4cbe36026a511fd6181b0f
이종호 <purepleya@gmail.com>
2024-09-05 22:26:48 +0900

    383. 지연 로딩 추가하기

--- 23 Deploying React Apps/src/App.js
+++ 23 Deploying React Apps/src/App.js
@@ -1,9 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
 import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
+import { lazy, Suspense } from 'react';
+
+const BlogPage = lazy(() => import('./pages/Blog'));
 
 const router = createBrowserRouter([
   {
@@ -17,7 +20,12 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
+          // { index: true, element: <BlogPage />, loader: postsLoader },
+          {
+            index: true,
+            element: (<Suspense fallback={<p>Loadking...</p>}><BlogPage /></Suspense>),
+            loader: () => import('./pages/Blog').then(module => module.loader()) 
+          },
           { path: ':id', element: <PostPage />, loader: postLoader },
         ],
       },
