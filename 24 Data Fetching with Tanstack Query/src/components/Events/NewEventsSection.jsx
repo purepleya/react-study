5d74ccef12b02573af6c00078c4d268a68adb656
이종호 <purepleya@gmail.com>
2024-09-08 19:29:17 +0900

    390. Tanstack 쿼리를 설치하고 사용하는 방법과 유용한 이점

--- 24 Data Fetching with Tanstack Query/src/components/Events/NewEventsSection.jsx
+++ 24 Data Fetching with Tanstack Query/src/components/Events/NewEventsSection.jsx
@@ -1,52 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from "@tanstack/react-query";
 
-import LoadingIndicator from '../UI/LoadingIndicator.jsx';
-import ErrorBlock from '../UI/ErrorBlock.jsx';
-import EventItem from './EventItem.jsx';
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+import { fetchEvents } from "../../util/http.js";
 
 export default function NewEventsSection() {
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:3000/events');
-
-      if (!response.ok) {
-        const error = new Error('An error occurred while fetching the events');
-        error.code = response.status;
-        error.info = await response.json();
-        throw error;
-      }
-
-      const { events } = await response.json();
-
-      return events;
-    }
-
-    fetchEvents()
-      .then((events) => {
-        setData(events);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
+  // data 에는 queryFn 으로 처리한 결과 값
+  // isPending 요청상태
+  // isError 에러 응답을 받았는지 여부
+  // 에러가 발생했을때의 에러 객체
+  // 기타 등등의 값들이 있음
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events'], // 키의 수나 데이터 타입이 정해져 있지 않다, 임의로 정하면 된다.
+    queryFn: fetchEvents,
+  });
 
   let content;
 
-  if (isLoading) {
+  if (isPending) {
     content = <LoadingIndicator />;
   }
 
-  if (error) {
+  if (isError) {
     content = (
-      <ErrorBlock title="An error occurred" message="Failed to fetch events" />
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events."}
+      />
     );
   }
 
