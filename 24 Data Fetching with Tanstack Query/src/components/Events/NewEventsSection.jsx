e839cc67b8d93687b3a2bbfac50265266b496544
이종호 <purepleya@gmail.com>
2024-09-10 19:59:41 +0900

    405. 쿼리 키를 쿼리 함수 입력으로 사용

--- 24 Data Fetching with Tanstack Query/src/components/Events/NewEventsSection.jsx
+++ 24 Data Fetching with Tanstack Query/src/components/Events/NewEventsSection.jsx
@@ -12,8 +12,8 @@ export default function NewEventsSection() {
   // 에러가 발생했을때의 에러 객체
   // 기타 등등의 값들이 있음
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ['events'], // 키의 수나 데이터 타입이 정해져 있지 않다, 임의로 정하면 된다.
-    queryFn: fetchEvents,
+    queryKey: ['events', {max: 3}], // 키의 수나 데이터 타입이 정해져 있지 않다, 임의로 정하면 된다.
+    queryFn: ({signal, queryKey}) => fetchEvents({signal, ...queryKey[1]}), // queryFn의 두번째 파라미터로 queryKey를 지정할 수 있다.
     staleTime: 5000, // 캐시 유지 시간(ms), 해당 시간 이전까지는 요청을 보내지 않는다. 기본은 0
     // gcTime: 1000 // 데이터와 캐시를 얼마나 유지할지(ms), 기본 5분, 이시간이 지나면 캐시를 지워버림
   });
