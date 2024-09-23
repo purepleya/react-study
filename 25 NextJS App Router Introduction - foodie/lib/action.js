f489ce6e17505aef2a79c55dc5b958d8db5c2112
이종호 <purepleya@gmail.com>
2024-09-23 22:04:16 +0900

    444. 업로드 된 이미지 저장 및 데이터베이스에 데이터 저장

--- 25 NextJS App Router Introduction - foodie/lib/action.js
+++ 25 NextJS App Router Introduction - foodie/lib/action.js
@@ -1,5 +1,8 @@
 'use server';
 
+import { redirect } from "next/navigation";
+import { saveMeal } from "./meals";
+
 export async function shareMeal(formData) {
   'use server';
 
@@ -12,5 +15,6 @@ export async function shareMeal(formData) {
     creator_email: formData.get('email')
   }
 
-  console.log(meal);
+  await saveMeal(meal);
+  redirect('/meals');
 }
