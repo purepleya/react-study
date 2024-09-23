ae25c5d3635cd2a30ef7ef7fd92e04c491af2a46
이종호 <purepleya@gmail.com>
2024-09-23 22:23:34 +0900

    446. 서버 사이드 입력 유효성 확인 추가 방법

--- 25 NextJS App Router Introduction - foodie/lib/action.js
+++ 25 NextJS App Router Introduction - foodie/lib/action.js
@@ -1,20 +1,36 @@
-'use server';
+"use server";
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+function isInvalidText(text) {
+  return !test || text.trim() === "";
+}
+
 export async function shareMeal(formData) {
-  'use server';
+  "use server";
 
   const meal = {
-    title: formData.get('title'),
-    summary: formData.get('summary'),
-    instructions: formData.get('instructions'),
-    image: formData.get('image'),
-    creator: formData.get('name'),
-    creator_email: formData.get('email')
+    title: formData.get("title"),
+    summary: formData.get("summary"),
+    instructions: formData.get("instructions"),
+    image: formData.get("image"),
+    creator: formData.get("name"),
+    creator_email: formData.get("email"),
+  };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.includes('@') ||
+    !meal.image || meal.image.size === 0
+  ) {
+    throw new Error('Invalid input')
   }
 
   await saveMeal(meal);
-  redirect('/meals');
+  redirect("/meals");
 }
