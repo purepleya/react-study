8d3eb3929bd3e8a05a1a88aa7ba6626f61e5a2be
이종호 <purepleya@gmail.com>
2024-09-27 23:38:17 +0900

    449. 캐시 유효성 재확인 트리거

--- 25 NextJS App Router Introduction - foodie/lib/action.js
+++ 25 NextJS App Router Introduction - foodie/lib/action.js
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 function isInvalidText(text) {
   return !test || text.trim() === "";
@@ -34,5 +35,6 @@ export async function shareMeal(prevState, formData) {
   }
 
   await saveMeal(meal);
+  revalidatePath('/meals');
   redirect("/meals");
 }
