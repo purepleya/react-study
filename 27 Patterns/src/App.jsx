a1865801be316a73d18141cdd3deb3dce20caece
이종호 <purepleya@gmail.com>
2024-10-04 22:16:55 +0900

    510. 컨텍스트 API로 멀티 컴포넌트 상태 관리하기

--- 27 Patterns/src/App.jsx
+++ 27 Patterns/src/App.jsx
@@ -7,13 +7,13 @@ function App() {
       <h2>Why work with us?</h2>
 
       <Accordion className="accordion">
-        <AccordionItem className="accordion-item" title="We got 20 years of experience">
+        <AccordionItem id="experience" className="accordion-item" title="We got 20 years of experience">
           <article>
             <p>You can&spos;t go wrong with us.</p>
             <p>We ar in the business of planning highly individualized vacation trips for more than 20 years.</p>
           </article>
         </AccordionItem>
-        <AccordionItem className="accordion-item" title="We're working with local guides">
+        <AccordionItem id="local-guides" className="accordion-item" title="We're working with local guides">
           <article>
             <p>We are not doing this along from our office.</p>
             <p>Instead, we are workign with local guides to ensure a safe and pleasant vacation.</p>
