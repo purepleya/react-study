1d0225c4cbf74a4c8f0fd899410a7433e8862fc9
이종호 <purepleya@gmail.com>
2024-10-09 23:41:13 +0900

    513. 컴파운드 컴포넌트로 작업할 때 교차 컴포넌트 상태 공유

--- 27 Patterns/src/App.jsx
+++ 27 Patterns/src/App.jsx
@@ -6,18 +6,18 @@ function App() {
       <h2>Why work with us?</h2>
 
       <Accordion className="accordion">
-        <Accordion.Item className="accordion-item">
-          <Accordion.Title className="accordion-item-title" id="experience">We got 20 years of experience</Accordion.Title>
-          <Accordion.Content className="accordion-item-content" id="experience">
+        <Accordion.Item id="experience" className="accordion-item">
+          <Accordion.Title className="accordion-item-title">We got 20 years of experience</Accordion.Title>
+          <Accordion.Content className="accordion-item-content">
             <article>
               <p>You can&spos;t go wrong with us.</p>
               <p>We ar in the business of planning highly individualized vacation trips for more than 20 years.</p>
             </article>
           </Accordion.Content>
         </Accordion.Item>
-        <Accordion.Item className="accordion-item">
-          <Accordion.Title className="accordion-item-title" id="local-guides">We're working with local guides</Accordion.Title>
-          <Accordion.Content className="accordion-item-content" id="local-guides">
+        <Accordion.Item id="local-guides" className="accordion-item">
+          <Accordion.Title className="accordion-item-title">We're working with local guides</Accordion.Title>
+          <Accordion.Content className="accordion-item-content">
             <article>
               <p>We are not doing this along from our office.</p>
               <p>Instead, we are workign with local guides to ensure a safe and pleasant vacation.</p>
