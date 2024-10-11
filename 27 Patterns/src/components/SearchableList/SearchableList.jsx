1346fcd14bae657f9858eaec98865ab4b13a49be
이종호 <purepleya@gmail.com>
2024-10-11 19:58:11 +0900

    디바운싱 작업하기

--- 27 Patterns/src/components/SearchableList/SearchableList.jsx
+++ 27 Patterns/src/components/SearchableList/SearchableList.jsx
@@ -1,12 +1,20 @@
+import { useRef } from "react";
 import { useState } from "react";
 
 export default function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef();
   const [searchTerm, setSearchTerm] = useState('');
 
   const searchResults = items.filter(item => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()));
 
   function handleChange(event) {
-    setSearchTerm(event.target.value);
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSearchTerm(event.target.value);      
+    }, 500);
   }
 
   return (
