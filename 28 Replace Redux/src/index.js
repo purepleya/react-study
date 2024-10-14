04357103980e521980dc19d055c2b7e9cc59b957
이종호 <purepleya@gmail.com>
2024-10-15 07:10:59 +0900

    522. 대안: 컨텍스트 API 사용

--- 28 Replace Redux/src/index.js
+++ 28 Replace Redux/src/index.js
@@ -1,24 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
+import ProductProvider from './context/product-context';
 
-const rootReducer = combineReducers({
-  shop: productReducer
-});
-
-const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider store={store}>
+  <ProductProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
+    </ProductProvider>,
   document.getElementById('root')
 );
