2490df4a5cf10a4341d7cd7289772a87131f4a5a
이종호 <purepleya@gmail.com>
2024-10-15 09:38:29 +0900

    523. 컨텍스트 API로 즐겨찾기 전환하기

--- 28 Replace Redux/src/components/Products/ProductItem.js
+++ 28 Replace Redux/src/components/Products/ProductItem.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+import { ProductsContext } from '../../context/product-context';
 
 const ProductItem = props => {
   
+  const toggleFav = useContext(ProductsContext).toggleFav;
   const toggleFavHandler = () => {
-    // dispatch(toggleFav(props.id));
+    toggleFav(props.id);
   };
 
   return (
