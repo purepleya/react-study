fa1fb7e5e2d93469cee9d6eedc51bb8b3dd22fc5
이종호 <purepleya@gmail.com>
2024-10-23 15:48:07 +0900

    541. 테스트 스위트와 함께 테스트 그룹화하기

--- 29 React App Test/src/components/Greeting.test.js
+++ 29 React App Test/src/components/Greeting.test.js
@@ -1,14 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders Hello World as a text", () => {
-  // Arrange
-  render(<Greeting />);
+describe('Greeting component', () => {
+  
+  test("renders Hello World as a text", () => {
+    // Arrange
+    render(<Greeting />);
+  
+    // Act
+    // ... nothing
+  
+    //Assert
+    const helloWorldElement = screen.getByText('Hello World!');
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 
-  // Act
-  // ... nothing
-
-  //Assert
-  const helloWorldElement = screen.getByText('Hello World!');
-  expect(helloWorldElement).toBeInTheDocument();
 });
+
