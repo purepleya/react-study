f02bcb27a03ff58d56ffdf797d653dff05b2ba31
이종호 <purepleya@gmail.com>
2024-10-29 20:14:35 +0900

    583. 다른 컴포넌트 만들어보기

--- 31 React Crash Course/src/App.jsx
+++ 31 React Crash Course/src/App.jsx
@@ -1,10 +1,9 @@
-import Post from "./components/Post";
+import PostList from "./components/PostList";
 
 function App() {
   return (
     <main>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manuel" body="Check out the full course!"/>
+      <PostList />
     </main>
   );
 }
