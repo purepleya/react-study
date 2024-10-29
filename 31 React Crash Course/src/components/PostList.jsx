69e55b8e83f6d1366802ac1e66191ae67b061c69
이종호 <purepleya@gmail.com>
2024-10-29 20:19:21 +0900

    584. 상태 이용을 위한 준비하기

--- 31 React Crash Course/src/components/PostList.jsx
+++ 31 React Crash Course/src/components/PostList.jsx
@@ -1,13 +1,17 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 
-import classes from './PostList.module.css';
+import classes from "./PostList.module.css";
 
 function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Maximilian" body="React.js is awesome!" />
-      <Post author="Manuel" body="Check out the full course!"/>
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
   );
 }
 
