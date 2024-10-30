590decbf45f375d643dbf859856516608da8daae
이종호 <purepleya@gmail.com>
2024-10-31 00:30:48 +0900

    589. 상태를 활용한 조건부 콘텐츠 로딩

--- 31 React Crash Course/src/components/Modal.jsx
+++ 31 React Crash Course/src/components/Modal.jsx
@@ -1,10 +1,10 @@
 import classes from './Modal.module.css';
 
-function Modal({ children }) {
+function Modal({ children, onClose }) {
   return (
     <>
-      <div className={classes.backdrop} />
-      <dialog open className={classes.backdrop} >
+      <div className={classes.backdrop} onClick={onClose} />
+      <dialog open className={classes.modal} >
         {children}
       </dialog>
     </>
