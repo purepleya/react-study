2828cff49fbff2034b8e27047c07eb0be1ecf358
이종호 <purepleya@gmail.com>
2024-10-31 14:45:35 +0900

    592. 양식 제출 처리

--- 31 React Crash Course/src/components/PostList.jsx
+++ 31 React Crash Course/src/components/PostList.jsx
@@ -6,18 +6,7 @@ import Modal from "./Modal";
 import classes from "./PostList.module.css";
 
 function PostList({isPosting, onStopPosting}) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
   
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
 
   // let modalContent;
 
@@ -35,15 +24,10 @@ function PostList({isPosting, onStopPosting}) {
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Manuel" body="Check out the full course!" />
       </ul>
     </>
