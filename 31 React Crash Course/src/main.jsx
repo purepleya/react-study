bb45f858f1d04d28a8402344101c55e738964206
이종호 <purepleya@gmail.com>
2024-11-01 10:32:25 +0900

    600. 라우트 추가하기

--- 31 React Crash Course/src/main.jsx
+++ 31 React Crash Course/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "./index.css";
+import NewPost from "./components/NewPost";
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />},
+  {path: '/create-post', element: <NewPost />},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
