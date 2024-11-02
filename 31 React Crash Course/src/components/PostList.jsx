e0279dbf90abbf22af0b3cb68037a97058e5d209
이종호 <purepleya@gmail.com>
2024-11-02 20:51:39 +0900

    604. loader()로 데이터 가져오기

--- 31 React Crash Course/src/components/PostList.jsx
+++ 31 React Crash Course/src/components/PostList.jsx
@@ -1,27 +1,11 @@
-import { useEffect, useState } from "react";
-import Post from "./Post";
+import { useLoaderData } from "react-router-dom";
 
+import Post from "./Post";
 import classes from "./PostList.module.css";
 
-function PostList({ isPosting, onStopPosting }) {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
+function PostList() {
+  const posts = useLoaderData();
   
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      const response = await fetch('http://localhost:8080/posts');
-      const resData = await response.json();
-      if (!response.ok) {
-
-      }
-      setPosts(resData.posts);
-      setIsFetching(false);
-    }
-
-    fetchPosts();
-  }, []);
-
   function addPostHandler(postData) {
     fetch('http://localhost:8080/posts', {
       method: 'POST',
@@ -42,15 +26,10 @@ function PostList({ isPosting, onStopPosting }) {
           ))}
         </ul>
       )}
-      {!isFetching && posts.length === 0 && (
+      {posts.length === 0 && (
         <div style={{ textAlign: 'center', color: 'white'}}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
-        </div>
-      )}
-      {isFetching && (
-        <div style={{ textAlign: 'center', color: 'white'}}>
-          <p>Loading posts...</p>
         </div>
       )}
     </>
--- 31 React Crash Course/src/main.jsx
+++ 31 React Crash Course/src/main.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Posts from "./routes/Posts";
+import Posts, { loader as postsLoader} from "./routes/Posts";
 
 import "./index.css";
 import NewPost from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
 
 const router = createBrowserRouter([
-  {path: '/', element: <RootLayout />, children: [
-    {path: '/', element: <Posts />, children: [
-      {path: '/create-post', element: <NewPost />},
-    ]},
-  ]},
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        loader: postsLoader,
+        children: [{ path: "/create-post", element: <NewPost /> }],
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
